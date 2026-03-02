ffaeafbf6a7629de6c62b47658259dacc4c59458
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-02 12:37:40 +0200

    removed the routers in the app component

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import About from "../src/component/About.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
