babc7231a26f4b7173df94ec3d949290b00d615b
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-02 22:24:00 +0200

    importing the home and about pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import About from "../src/component/About.jsx";
 import Home from "./component/Home.jsx";
-
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-      <Link className="text-2xl font-bold p-3" to='/'>Home</Link>
-      <Link className="text-2xl font-bold p-3" to='/about'>About</Link>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="w-140 h-fit mt-10 flex flex-col justify-center items-center mx-auto">
+        {/* <Home /> */}
+
+        <About />
+      </div>
     </>
   );
 }
