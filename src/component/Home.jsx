d59a7b51b4ede9cc5389d33f71dc81a714d26fe9
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-02 22:23:28 +0200

    the complete home component

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,26 @@
-export default function Home(){
-    return (
-        <>
-            <h1>This is the home page of the application</h1>
-        </>
-    );
+import NavBar from "./Navbar.jsx";
+import Footer from "./Footer.jsx";
+export default function Home() {
+  return (
+    <>
+      <div className="w-full h-full flex flex-col justify-center items-center mx-auto">
+        <NavBar />
+        <div className="w-140 h-full bg-[url('/imgs.png')] bg-cover bg-center text-white">
+          <div className="flex flex-col justify-between items-center space-y-10 p-4">
+            <h1 className="font-extrabold mt-15 text-[40px] font-[inter]">
+              You got the travel plans, we got the travel vans.
+            </h1>
+            <p className="text-[18px] font-[inter] font-normal">
+              Add adventure to your life by joining the #vanlife movement. Rent
+              the perfect van to make your perfect road trip.
+            </p>
+            <button className="w-100 h-10 rounded-sm bg-[#FF8C38] flex  justify-center items-center font-semibold">
+              Find your van
+            </button>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
