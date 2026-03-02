a0619098dcb6f2cac50a610a3dcb7b1af3ea400c
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-02 12:37:12 +0200

    imported the home and about components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import About from "../src/component/About.jsx";
+import Home from "./component/Home.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <h1>Hello world</h1>
+      <BrowserRouter>
+      <Link className="text-2xl font-bold p-3" to='/'>Home</Link>
+      <Link className="text-2xl font-bold p-3" to='/about'>About</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
