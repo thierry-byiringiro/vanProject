06fcb5917391f241b2620f45ad1c1cf5d8f21e62
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-02 22:23:03 +0200

    the about page of the project

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,7 +1,33 @@
-export default function About(){
-    return (
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+
+export default function About() {
+  return (
     <>
-        <h1>Hello, I'm christian oronaldo</h1>
+      <div className="w-full h-full flex flex-col justify-center items-center mx-auto font-[inter] bg-white text-black ">
+        <NavBar />
+        <div className="w-full h-full  flex flex-col">
+          <img src="/imgas.png" alt="" />
+          <div className="flex flex-col p-5 space-y-5">
+            <h1 className=" font-extrabold text-[33px] leading-tight">
+              Don’t squeeze in a sedan when you could relax in a van.
+            </h1>
+            <div className="flex flex-col space-y-6 font-medium" >
+              <p className="text-[16px] ">
+                Our mission is to enliven your road trip with the perfect travel
+                van rental. Our vans are recertified before each trip to ensure
+                your travel plans can go off without a hitch. (Hitch costs extra
+                😉
+              </p>
+              <p>
+                Our team is full of vanlife enthusiasts who know firsthand the
+                magic of touring the world on 4 wheels.
+              </p>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </>
-    )
+  );
 }
