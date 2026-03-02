85b71892f9b8148a7f6b00a90de156dd838a9e6b
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-02 22:22:20 +0200

    the navbar component

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,20 +2,12 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 export default function NavBar({ Home, About }) {
   return (
     <>
-      <div className="flex flex-row justify-between w-full">
+      <div className="w-full flex flex-row justify-between items-center p-4 h-25 bg-[#FFF7ED]">
         <h1 className="font-bold text-2xl">#VANLIFE</h1>
-        <BrowserRouter className="flex flex-row text-lg font-bold ">
-          <Link className="" to="/">
-            Home
-          </Link>
-          <Link className="" to="/about">
-            About
-          </Link>
-          <Routes >
-            <Route path="/" element={Home} />
-            <Route path="/about" element={About} />
-          </Routes>
-        </BrowserRouter>
+        <div className="flex text-lg font-bold space-x-10">
+          <button>Home</button>
+          <button>About</button>
+        </div>
       </div>
     </>
   );
