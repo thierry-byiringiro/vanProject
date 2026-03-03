040fca18832feebbe6ea10ce181eac72752af992
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-03 18:32:10 +0200

    imported the about page

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import "./App.css";
 import ReactDOM from "react-dom/client";
 import About from "../src/component/About.jsx";
 import Home from "./component/Home.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Test from "./component/test.jsx";
 
 function App() {
   return (
     <>
       <div className="w-140 h-fit mt-10 flex flex-col justify-center items-center mx-auto">
-        {/* <Home /> */}
-
-        <About />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}/>  
+            <Route path="/about" element={<About/>}/>  
+          </Routes>          
+        </BrowserRouter>
       </div>
+      <Test/>
     </>
   );
 }
