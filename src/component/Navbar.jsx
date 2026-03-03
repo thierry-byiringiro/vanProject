b83a28320adec45fc2a31230c456118037122862
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-03 18:30:10 +0200

    added the navbars to link to the pages

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,10 +3,16 @@ export default function NavBar({ Home, About }) {
   return (
     <>
       <div className="w-full flex flex-row justify-between items-center p-4 h-25 bg-[#FFF7ED]">
-        <h1 className="font-bold text-2xl">#VANLIFE</h1>
+        <Link to="/">
+          <h1 className="font-bold text-2xl">#VANLIFE</h1>
+        </Link>
         <div className="flex text-lg font-bold space-x-10">
-          <button>Home</button>
-          <button>About</button>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+          <Link to="/about">
+            <button>Vans</button>
+          </Link>
         </div>
       </div>
     </>
