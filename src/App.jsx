9ad0ebc260b40d6868f804d0105e14080ce2f650
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-04 18:39:54 +0200

    updated the imported and exports

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import ReactDOM from "react-dom/client";
-import About from "../src/component/About.jsx";
-import Home from "./component/Home.jsx";
-import Vans from "./component/Vans.jsx";
-import VanDetails from "./component/VanDetails.jsx";
+import About from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
+import Vans from "./pages/vans/Vans.jsx";
+import VanDetails from "./pages/vans/VanDetails.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./component/Layout.jsx";
+import Layout from "./pages/vans/Layout.jsx";
+import Dashboard from "./pages/host/Dashboard.jsx";
+import Income from "./pages/host/Income.jsx";
+import Reviews from "./pages/host/Reviews.jsx";
+import HostLayout from "./component/HostLayout.jsx";
 
 function App() {
   return (
@@ -18,9 +22,14 @@ function App() {
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/vans" element={<Vans />} />
-              <Route path="/vans/:id" element={<VanDetails />} />
+              <Route path="about" element={<About />} />
+                <Route path="vans" element={<Vans />} />
+                <Route path="vans/:id" element={<VanDetails />} />
+              <Route path="host" element={<HostLayout />}>
+                <Route index element={<Dashboard />} />
+                <Route path="income" element={<Income />} />
+                <Route path="reviews" element={<Reviews />} />
+              </Route>
             </Route>
           </Routes>
         </BrowserRouter>
