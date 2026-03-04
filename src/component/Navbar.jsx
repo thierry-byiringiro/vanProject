c97625ad3d8088fd5af77c0a090e9158fe4db57c
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-04 15:41:20 +0200

    changed from Link to Navlinks

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Link, Route, Routes, NavLink } from "react-router-dom";
-export default function NavBar({ Home, About }) {
+export default function NavBar() {
   return (
     <>
       <div className="w-full flex flex-row justify-between items-center p-4 h-25 bg-[#FFF7ED]">
-        <Link to="/">
+        <NavLink to="/" className="hover:underline">
           <h1 className="font-bold text-2xl">#VANLIFE</h1>
-        </Link>
+        </NavLink>
         <div className="flex text-lg font-bold space-x-10">
-          <NavLink to="/"></NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/vans">Vans</NavLink>
+          <NavLink to="/" className="hover:underline"></NavLink>
+          <NavLink to="/about" className="hover:underline">About</NavLink>
+          <NavLink to="/vans" className="hover:underline">Vans</NavLink>
         </div>
       </div>
     </>
