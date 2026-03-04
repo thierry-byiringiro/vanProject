c3a9801f5ad2d7d44f06c5b7a014af24d072202b
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-04 15:42:34 +0200

    wrapped all the routes into one big route

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import ReactDOM from "react-dom/client";
 import About from "../src/component/About.jsx";
 import Home from "./component/Home.jsx";
 import Vans from "./component/Vans.jsx";
+import VanDetails from "./component/VanDetails.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./component/Layout.jsx";
 
 function App() {
   return (
@@ -14,10 +16,13 @@ function App() {
       <div className="w-140 h-fit mt-10 flex flex-col justify-center items-center mx-auto bg-white">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home/>}/>  
-            <Route path="/about" element={<About/>}/>
-            <Route path="/vans" element={<Vans />} />
-          </Routes>          
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/vans" element={<Vans />} />
+              <Route path="/vans/:id" element={<VanDetails />} />
+            </Route>
+          </Routes>
         </BrowserRouter>
       </div>
     </>
