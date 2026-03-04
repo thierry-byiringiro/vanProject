316c7206cfd578c940aa4412c798446c6173f70f
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-04 11:43:26 +0200

    added the vans component

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ import "./App.css";
 import ReactDOM from "react-dom/client";
 import About from "../src/component/About.jsx";
 import Home from "./component/Home.jsx";
+import Vans from "./component/Vans.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Test from "./component/test.jsx";
 
 function App() {
   return (
     <>
-      <div className="w-140 h-fit mt-10 flex flex-col justify-center items-center mx-auto">
+      <div className="w-140 h-fit mt-10 flex flex-col justify-center items-center mx-auto bg-white">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home/>}/>  
-            <Route path="/about" element={<About/>}/>  
+            <Route path="/about" element={<About/>}/>
+            <Route path="/vans" element={<Vans />} />
           </Routes>          
         </BrowserRouter>
       </div>
-      <Test/>
     </>
   );
 }
