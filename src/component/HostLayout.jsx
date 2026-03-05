be394f0a8a163ce463eef4cc627e70cc5d16b13d
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-05 02:11:58 +0200

    added the host route

--- src/component/HostLayout.jsx
+++ src/component/HostLayout.jsx
@@ -4,9 +4,39 @@ export default function HostLayout() {
   return (
     <>
       <nav className="flex flex-row space-x-8 font-bold w-full justify-start py-4 px-6">
-        <NavLink to="/host" className={({isActive}) => `${isActive ? "underline text-green-600":""}`}>Dashboard</NavLink>
-        <NavLink to="/host/income" className={({isActive}) => `${isActive ? "underline text-green-600":""}`}>Income</NavLink>
-        <NavLink to="/host/reviews" className={({isActive}) => `${isActive ? "underline text-green-600":""}`}>Reviews</NavLink>
+        <NavLink
+          to=". "
+          end
+          className={({ isActive }) =>
+            `${isActive ? "underline text-green-600" : ""}`
+          }
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="vans"
+          className={({ isActive }) =>
+            `${isActive ? "underline text-green-600" : ""}`
+          }
+        >
+          Vans
+        </NavLink>
+        <NavLink
+          to="income"
+          className={({ isActive }) =>
+            `${isActive ? "underline text-green-600" : ""}`
+          }
+        >
+          Income
+        </NavLink>
+        <NavLink
+          to="reviews"
+          className={({ isActive }) =>
+            `${isActive ? "underline text-green-600" : ""}`
+          }
+        >
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </>
