337b43ac8eb216fc2762edfa23d4b81925261e9e
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-06 17:49:52 +0200

    moved the server at the top of the page

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
+import '../server.js' 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import '/server.js' 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
