37446e4cbb673cb00ae7c2b7f4a570ba32052614
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-07 22:20:19 +0200

    changed from using useEffect to using loader

--- src/pages/vans/VanDetails.jsx
+++ src/pages/vans/VanDetails.jsx
@@ -1,15 +1,17 @@
-import { useParams, NavLink, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-
+import {
+  useParams,
+  NavLink,
+  useLocation,
+  useLoaderData,
+} from "react-router-dom";
+import { getVans } from "../../assets/api";
+export async function loader({ params }) {
+  const data = await getVans(params.id);
+  return data;
+}
 export default function VanDetails() {
-  const [vanDetail, setVanDetail] = useState({});
-  const params = useParams();
   const location = useLocation();
-  useEffect(() => {
-    fetch(`/api/vans/${params.id}`)
-      .then((respose) => respose.json())
-      .then((data) => setVanDetail(data.vans));
-  }, [params.id]);
+  const vanDetail = useLoaderData();
   let elType;
   if (vanDetail) {
     elType =
@@ -23,30 +25,31 @@ export default function VanDetails() {
   return (
     <>
       <NavLink to={`${search}`} relative="path" className="back-button">
-        &larr; {location.state.searchP ? (<span>Back to {vanDetail.type} vans</span>) : (<span>Back to all vans</span>)}
+        &larr;{" "}
+        {location.state.searchP ? (
+          <span>Back to {vanDetail.type} vans</span>
+        ) : (
+          <span>Back to all vans</span>
+        )}
       </NavLink>
-      {vanDetail ? (
-        <div className="flex flex-col p-6 space-y-5">
-          <img src={vanDetail.imageUrl} className="rounded-md" />
-          <button
-            className={`selected w-[80.71px] h-[30.89px] rounded-md text-white p-2 flex justify-center items-center  font-semibold`}
-            style={{ backgroundColor: elType }}
-          >
-            {String(vanDetail.type).charAt(0).toUpperCase() +
-              String(vanDetail.type).slice(1)}
-          </button>
-          <h2 className=" font-bold">{vanDetail.name}</h2>
-          <p className="">
-            <span className=" font-bold">${vanDetail.price}</span>/day
-          </p>
-          <p className="font-semibold">{vanDetail.description}</p>
-          <button className="w-130 h-10 rounded-sm bg-[#FF8C38] flex  justify-center items-center font-semibold text-white">
-            Rent this van
-          </button>
-        </div>
-      ) : (
-        <h2>Loading...</h2>
-      )}
+      <div className="flex flex-col p-6 space-y-5">
+        <img src={vanDetail.imageUrl} className="rounded-md" />
+        <button
+          className={`selected w-[80.71px] h-[30.89px] rounded-md text-white p-2 flex justify-center items-center  font-semibold`}
+          style={{ backgroundColor: elType }}
+        >
+          {String(vanDetail.type).charAt(0).toUpperCase() +
+            String(vanDetail.type).slice(1)}
+        </button>
+        <h2 className=" font-bold">{vanDetail.name}</h2>
+        <p className="">
+          <span className=" font-bold">${vanDetail.price}</span>/day
+        </p>
+        <p className="font-semibold">{vanDetail.description}</p>
+        <button className="w-130 h-10 rounded-sm bg-[#FF8C38] flex  justify-center items-center font-semibold text-white">
+          Rent this van
+        </button>
+      </div>
     </>
   );
 }
