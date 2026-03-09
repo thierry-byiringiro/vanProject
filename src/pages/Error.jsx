a7e84813dd04f438f851ffd71279be676df1e82f
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-09 16:48:53 +0200

    added the check so that the loaders can work

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,12 +3,18 @@ import { useRouteError } from "react-router-dom";
 export default function Error() {
   const error = useRouteError();
 
+  if (error && error.redirectTo) {
+    window.location.href = error.redirectTo;
+    return null;
+  }
   return (
     <div className="error-container">
       <h1 className="font-[inter] text-2xl">
-        {error.message || "An unexpected error occurred"}
+        {error.statusText || error.message || "An unexpected error occurred"}
       </h1>
-      <pre>{error.status} - {error.statusText}</pre>
+      <pre>
+        {error.status} {error.status && "-"} {error.data || "Check console for details"}
+      </pre>
     </div>
   );
 }
