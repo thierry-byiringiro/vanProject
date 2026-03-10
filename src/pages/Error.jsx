73205222c7b83c3a5022e4d4fc3b2a81145f989f
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-10 23:04:56 +0200

    changing if the error contains the redirectTo property

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,19 +1,19 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Navigate } from "react-router-dom";
 
 export default function Error() {
   const error = useRouteError();
 
   if (error && error.redirectTo) {
-    window.location.href = error.redirectTo;
-    return null;
+    return <Navigate to={error.redirectTo} replace />;
   }
+
   return (
     <div className="error-container">
       <h1 className="font-[inter] text-2xl">
-        {error.statusText || error.message || "An unexpected error occurred"}
+        {error?.statusText || error?.message || "An unexpected error occurred"}
       </h1>
       <pre>
-        {error.status} {error.status && "-"} {error.data || "Check console for details"}
+        {error?.status} {error?.status && "-"} {error?.data || "Check console for details"}
       </pre>
     </div>
   );
