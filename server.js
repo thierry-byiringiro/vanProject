a12ee38486d05bac37c8d5b5c7ff4195898107e2
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-10 02:26:34 +0200

    added the routes to handle the authentication

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { createServer, Model, Response } from "miragejs";
 createServer({
   models: {
     vans: Model,
+    users: Model
   },
 
   seeds(server) {
@@ -72,6 +73,7 @@ createServer({
       type: "rugged",
       hostId: "123",
     });
+    server.create("user", { id: "123", email: "b@b.com", password: "p123", name: "Bob" })
   },
 
   routes() {
@@ -95,6 +97,24 @@ createServer({
     this.get("/host/vans/:id", (schema, request) => {
       const id = request.params.id;
       return schema.vans.findBy({ id, hostId: "123" });
+    });
+    this.post("/login", (schema, request) => {
+      const { email, password } = JSON.parse(request.requestBody);
+      const foundUser = schema.users.findBy({ email, password });
+      if (!foundUser) {
+        return new Response(
+          401,
+          {},
+          { message: "No user with those credentials found!" },
+        );
+      }
+
+      // At the very least, don't send the password back to the client 😅
+      foundUser.password = undefined;
+      return {
+        user: foundUser,
+        token: "Enjoy your pizza, here's your tokens.",
+      };
     });
     this.passthrough();
   },
