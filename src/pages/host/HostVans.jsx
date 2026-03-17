7b2e9e4100bd039b96518a00567ec7d53ccbaa1b
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-17 21:22:19 +0200

    changed from using the await and using the Await component and suspense

--- src/pages/host/HostVans.jsx
+++ src/pages/host/HostVans.jsx
@@ -1,33 +1,41 @@
-import { useEffect, useState } from "react";
-import { NavLink, useLoaderData } from "react-router-dom";
+import { Suspense, useEffect, useState } from "react";
+import { Await, NavLink, useLoaderData } from "react-router-dom";
 import { getHostVans } from "../../assets/api";
 import { requireAuth } from "../../assets/utils";
 export async function loader({ request }) {
   await requireAuth(request);
-  return await getHostVans();
+  return { hostvans: getHostVans() };
 }
 export default function HostVans() {
-  const hostVan = useLoaderData();
-  const getHostVans = hostVan.map((element) => (
-    <NavLink key={element.id} to={`${element.id}`}>
-      <div
-        key={element.id}
-        className="w-123.5 h-30.25 bg-[#FFFFFF] rounded-md flex flex-row items-center p-4 space-x-6"
-      >
-        <img src={element.imageUrl} alt="" className="w-25 h-25 rounded-md " />
-        <div>
-          <h1 className="font-bold">{element.name}</h1>$
-          <span>{element.price}</span>/day
-        </div>
-      </div>
-    </NavLink>
-  ));
-
+  const hostVanPromise = useLoaderData();
   return (
     <>
       <div className="flex flex-col space-y-6 p-4">
         <h1 className="font-bold text-3xl">Your listed Vans</h1>
-        {getHostVans}
+        <Suspense fallback={<h2>Loaing host vans...</h2>}>
+          <Await resolve={hostVanPromise.hostvans}>
+            {(loadedHostVans) => 
+              loadedHostVans.map((element) => (
+                <NavLink key={element.id} to={`${element.id}`}>
+                  <div
+                    key={element.id}
+                    className="w-123.5 h-30.25 bg-[#FFFFFF] rounded-md flex flex-row items-center p-4 space-x-6"
+                  >
+                    <img
+                      src={element.imageUrl}
+                      alt=""
+                      className="w-25 h-25 rounded-md "
+                    />
+                    <div>
+                      <h1 className="font-bold">{element.name}</h1>$
+                      <span>{element.price}</span>/day
+                    </div>
+                  </div>
+                </NavLink>
+              ))
+            }
+          </Await>
+        </Suspense>
       </div>
     </>
   );
